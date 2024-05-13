9edc2a5ef58e5d3b39df3261f87adbb6b87ccde8
Noah Forward <forwardnoah@gmail.com>
2024-05-13 21:07:18 -0230

    Table and Weight updated details A01

--- Assignment/A01/process_cargo.js
+++ Assignment/A01/process_cargo.js
@@ -5,33 +5,60 @@ headerLbl = headerLbl.substring(0,headerLbl.indexOf("XXXXX"));
 headerLbl += $("#BoxCarID").val();
 document.write(headerLbl);
 
-//Step 3 Create Table
+//Step 3 Create and add Table
 document.write("<table>");
 document.write("<tr><th>Transport ID</th><th>Description</th><th>Weight</th></tr>");
-document.write("<tr><td>XXXXX</td><td>XXXXX</td><td>XXXXX</td></tr>");
-document.write("<tr><td>XXXXX</td><td>XXXXX</td><td>XXXXX</td></tr>");
-document.write("<tr><td>XXXXX</td><td>XXXXX</td><td>XXXXX</td></tr>");
-document.write("<tr><td>XXXXX</td><td>XXXXX</td><td>XXXXX</td></tr>");
-document.write("<tr><td>XXXXX</td><td>XXXXX</td><td>XXXXX</td></tr>");
-document.write("<tr><td>Total Weight:</td><td>XXXXX</td></tr>")
+document.write("<tr id=\"0\"><td class=\"transportID\">XXXXX</td><td class=\"description\">XXXXX</td><td class=\"weight\">XXXXX</td></tr>");
+document.write("<tr id=\"1\"><td class=\"transportID\">XXXXX</td><td class=\"description\">XXXXX</td><td class=\"weight\">XXXXX</td></tr>");
+document.write("<tr id=\"2\"><td class=\"transportID\">XXXXX</td><td class=\"description\">XXXXX</td><td class=\"weight\">XXXXX</td></tr>");
+document.write("<tr id=\"3\"><td class=\"transportID\">XXXXX</td><td class=\"description\">XXXXX</td><td class=\"weight\">XXXXX</td></tr>");
+document.write("<tr id=\"4\"><td class=\"transportID\">XXXXX</td><td class=\"description\">XXXXX</td><td class=\"weight\">XXXXX</td></tr>");
+document.write("<tr><td>Total Cargo Weight:</td><td id=\"totalCargoWeight\">XXXXX</td></tr>")
 document.write("</table>");
 
+// Used to track current table row index
+var tableRowIndex = 0;
+
+// Add data to table and update weight values
 const processCargo = () => {
-    console.log("process");
+    const transportID = $("#TransportID").val();
+    const description = $("#Description").val();
+    const cargoWeight = $("#CargoWeight").val();
+
+    const currentTransportID = $("tr#" + tableRowIndex + " > td.transportID");
+    currentTransportID.text(transportID);
+
+    const currentDescription = $("tr#" + tableRowIndex + " > td.description");
+    currentDescription.text(description);
+
+    const currentCargoWeight = $("tr#" + tableRowIndex + " > td.weight");
+    currentCargoWeight.text(cargoWeight);
+
+    // Update weight values
+    let totalCargoWeight = 0;
+    if ($("#totalCargoWeight").text() == "XXXXX"){
+        totalCargoWeight = parseFloat(cargoWeight);
+    }
+    else {
+        totalCargoWeight = parseFloat($("#totalCargoWeight").text()) + parseFloat(cargoWeight);
+    }
+    $("#totalCargoWeight").text(totalCargoWeight);
+    $("#TotalWeight").val( parseFloat($("#EmptyWeight").val()) + totalCargoWeight);
+
+    tableRowIndex++;
 };
 
+// Clear data
 const resetForm = () => {
-    console.log("reset");
+    $("#TransportID").val("");
+    $("#Description").val("");
+    $("#CargoWeight").val("");
 };
 
+// Attach event listeners to process and reset buttons
 $("document").ready( () => {
     console.log("Ready!")
-
-    console.log($("#processCargoBtn"));
-    $("#processCargoBtn").addEventListener("click", processCargo);
-    //$("#resetFormBtn").addEventListener("click", resetForm);
-
-
-
-
+    
+    $("#processCargoBtn").click(processCargo);
+    $("#resetFormBtn").click(resetForm);
 });
