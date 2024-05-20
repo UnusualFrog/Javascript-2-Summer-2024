965773b4bc95e50987de08cc05f917fef542328b
Noah Forward <forwardnoah@gmail.com>
2024-05-20 14:11:35 -0230

    progress on a02

--- Assignment/A02/process_cargo.js
+++ Assignment/A02/process_cargo.js
@@ -1,29 +1,42 @@
-// Create and add label and table
-const generateTable = () => {
-    if ($("h3").length == 0){
-        // Header Label creation
-        let headerLbl = "<h3>Cargo Box Car Manifest for Box Car XXXXX</h3>";
-        headerLbl = headerLbl.substring(0,headerLbl.indexOf("XXXXX"));
-        headerLbl += $("#BoxCarID").val();
-        $("body").append(headerLbl);
+// Steps 1,2 & 3 Create and add label and table
+const generateTables = () => {
+    if ($("#output").length == 0){
+        $("body").append("<div id=\"output\"></div>");
 
-        // Table row headers and summary row created
-        let table = "<table id=\"BoxCarTable\">";
-        table += "<tr id=\"HeaderRow\"><th>Transport ID</th><th>Description</th><th>Weight</th></tr>";
-        table += "<tr id=\"SummaryRow\"><td>Total Cargo Weight:</td><td id=\"totalCargoWeight\">XXXXX</td></tr>";
-        table += "</table>";
-        $("body").append(table);
+        // Status Header Label creation
+        let statusLbl = "<h3>Cargo Status</h3>";
+        $("#output").append(statusLbl);
+
+        // Status Table row headers and summary row created
+        let statusTable = "<table id=\"StatusTable\">";
+        statusTable += "<tr id=\"HeaderRow\"><th>Transport ID</th><th>Description</th><th>Weight</th><th>Status</th></tr>";
+        statusTable += "</table>";
+        $("#output").append(statusTable);
+
+        // Manifest Header Label creation
+        let manifestLbl = "<h3>Manifest: XXXXX</h3>";
+        manifestLbl = manifestLbl.substring(0,manifestLbl.indexOf("XXXXX"));
+        manifestLbl += $("#BoxCarID").val();
+        $("#output").append(manifestLbl);
+
+        // Manifest Table row headers and summary row created
+        let manifestTable = "<table id=\"ManifestTable\">";
+        manifestTable += "<tr id=\"HeaderRow\"><th>Transport ID</th><th>Description</th><th>Weight</th></tr>";
+        manifestTable += "<tr id=\"SummaryRow\"><td>Total Cargo Weight:</td><td id=\"totalCargoWeight\">XXXXX</td></tr>";
+        manifestTable += "</table>";
+        $("#output").append(manifestTable);
     }
 }
 
-// Adds data to table and updates weight values
+// Adds cargo data to tables and updates weight values
 const processCargo = () => {
-    // Retrieve cargo data
+    // Retrieve cargo input data
     const transportID = $("#TransportID").val();
     const description = $("#Description").val();
     const cargoWeight = $("#CargoWeight").val();
+    let cargoStatus =  $("#TransportID").val();
 
-    // Check for blank values
+    // Check for invalid input
     if (transportID == ""){
         alert("Transport ID must not be blank");
     }
@@ -31,30 +44,48 @@ const processCargo = () => {
         alert("Description must not be blank");
     }
     else if (cargoWeight == ""){
-        alert("Cargo Weight must not be blank");
+        alert("Cargo weight must not be blank");
     }
-    else if (typeof cargoWeight === "number"){
-        alert("Cargo Weight must be a numeric value");
+    else if (isNaN(cargoWeight)){
+        alert("Cargo weight must be numeric");
+    }
+    else if (1 > cargoWeight){
+        alert("Cargo weight must be at least 1");
+    }
+    else if (cargoWeight > (parseFloat($("#MaxWeight").val()) - parseFloat($("#EmptyWeight").val()))) {
+        alert("Cargo must not exceed max weight");
     }
     else {
-        generateTable();
+        generateTables();
 
-        // Add row to table with cargo data
-        let row = `<tr id=\"0\"><td class=\"transportID\">${transportID}</td><td class=\"description\">${description}</td><td class=\"weight\">${cargoWeight}</td></tr>`;
-        $(row).insertBefore($("#SummaryRow"));
-
-        // Update weight values
+        // Update total weight and cargo weight values
         let totalCargoWeight = 0;
         if ($("#totalCargoWeight").text() == "XXXXX"){
             totalCargoWeight = parseFloat(cargoWeight);
         }
         else {
-            totalCargoWeight = parseFloat($("#totalCargoWeight").text()) + parseFloat(cargoWeight);
+           totalCargoWeight = parseFloat($("#totalCargoWeight").text()) + parseFloat(cargoWeight);
         }
-        $("#totalCargoWeight").text(totalCargoWeight);
-        $("#TotalWeight").val( parseFloat($("#EmptyWeight").val()) + totalCargoWeight);
-    }
+        
+        let totalCurrentWeight = parseFloat($("#EmptyWeight").val()) + totalCargoWeight
+        $("#CurrentWeight").val(totalCurrentWeight);
 
+        // Set cargo status based on max weight
+        if (totalCurrentWeight > $("#MaxWeight").val()) {
+            cargoStatus = "Warehouse";
+        }
+        else {
+            let manifestRow = `<tr><td class=\"transportID\">${transportID}</td><td class=\"description\">${description}</td><td class=\"weight\">${cargoWeight}</td></tr>`;
+            $(manifestRow).insertBefore($("#SummaryRow"));
+            $("#totalCargoWeight").text(totalCargoWeight);  
+        }   
+
+        // Add rows to table with cargo data
+        let statusRow = `<tr><td class=\"transportID\">${transportID}</td><td class=\"description\">${description}</td><td class=\"weight\">${cargoWeight}</td><td class=\"status\">${cargoStatus}</td></tr>`;
+        $("#StatusTable").append(statusRow);
+
+        
+    }
 
     
 };
@@ -70,7 +101,6 @@ const resetForm = () => {
 $("document").ready( () => {
     console.log("Ready!");
 
-    //$("#processCargoBtn").click(generateTable);
     $("#processCargoBtn").click(processCargo);
     $("#resetFormBtn").click(resetForm);
 });
