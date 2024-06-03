b6052c4282b01ff2f498cb9776a81d463331f372
Noah Forward <forwardnoah@gmail.com>
2024-06-03 17:15:07 -0230

    Finishing touches A02

--- Assignment/A02/process_cargo.js
+++ Assignment/A02/process_cargo.js
@@ -1,6 +1,6 @@
-// Steps 1,2 & 3 Create and add label and table
+// Create cargo status and manifest elements
 const generateTables = () => {
-    if ($("#output").length == 0){
+    if ($("#output").length == 0) {
         $("body").append("<div id=\"output\"></div>");
 
         // Status Header Label creation
@@ -15,7 +15,7 @@ const generateTables = () => {
 
         // Manifest Header Label creation
         let manifestLbl = "<h3>Manifest: XXXXX</h3>";
-        manifestLbl = manifestLbl.substring(0,manifestLbl.indexOf("XXXXX"));
+        manifestLbl = manifestLbl.substring(0, manifestLbl.indexOf("XXXXX"));
         manifestLbl += $("#BoxCarID").val();
         $("#output").append(manifestLbl);
 
@@ -31,25 +31,26 @@ const generateTables = () => {
 // Adds cargo data to tables and updates weight values
 const processCargo = () => {
     // Retrieve cargo input data
+    let boxCarId = $("#BoxCarID").val();
     const transportID = $("#TransportID").val();
     const description = $("#Description").val();
     const cargoWeight = $("#CargoWeight").val();
-    let cargoStatus =  $("#TransportID").val();
+    let cargoStatus = $("#TransportID").val();
 
     // Check for invalid input
-    if (transportID == ""){
+    if (transportID == "") {
         alert("Transport ID must not be blank");
     }
-    else if (description == ""){
+    else if (description == "") {
         alert("Description must not be blank");
     }
-    else if (cargoWeight == ""){
+    else if (cargoWeight == "") {
         alert("Cargo weight must not be blank");
     }
-    else if (isNaN(cargoWeight)){
+    else if (isNaN(cargoWeight)) {
         alert("Cargo weight must be numeric");
     }
-    else if (1 > cargoWeight){
+    else if (1 > cargoWeight) {
         alert("Cargo weight must be at least 1");
     }
     else if (cargoWeight > (parseFloat($("#MaxWeight").val()) - parseFloat($("#EmptyWeight").val()))) {
@@ -60,35 +61,29 @@ const processCargo = () => {
 
         // Update total weight and cargo weight values
         let totalCargoWeight = 0;
-        if ($("#totalCargoWeight").text() == "XXXXX"){
+        if ($("#totalCargoWeight").text() == "XXXXX") {
             totalCargoWeight = parseFloat(cargoWeight);
         }
         else {
-           totalCargoWeight = parseFloat($("#totalCargoWeight").text()) + parseFloat(cargoWeight);
+            totalCargoWeight = parseFloat($("#totalCargoWeight").text()) + parseFloat(cargoWeight);
         }
-        
         let totalCurrentWeight = parseFloat($("#EmptyWeight").val()) + totalCargoWeight
-        
 
         // Set cargo status based on max weight
         if (totalCurrentWeight > $("#MaxWeight").val()) {
-            cargoStatus = "Warehouse";
+            boxCarId = "Warehouse";
         }
         else {
             let manifestRow = `<tr><td class=\"transportID\">${transportID}</td><td class=\"description\">${description}</td><td class=\"weight\">${cargoWeight}</td></tr>`;
             $(manifestRow).insertBefore($("#SummaryRow"));
-            $("#totalCargoWeight").text(totalCargoWeight);  
+            $("#totalCargoWeight").text(totalCargoWeight);
             $("#CurrentWeight").val(totalCurrentWeight);
-        }   
+        }
 
         // Add rows to table with cargo data
-        let statusRow = `<tr><td class=\"transportID\">${transportID}</td><td class=\"description\">${description}</td><td class=\"weight\">${cargoWeight}</td><td class=\"status\">${cargoStatus}</td></tr>`;
+        let statusRow = `<tr><td class=\"transportID\">${transportID}</td><td class=\"description\">${description}</td><td class=\"weight\">${cargoWeight}</td><td class=\"status\">${boxCarId}</td></tr>`;
         $("#StatusTable").append(statusRow);
-
-        
     }
-
-    
 };
 
 // Clear data
@@ -98,10 +93,14 @@ const resetForm = () => {
     $("#CargoWeight").val("");
 };
 
-// Attach event listeners to process and reset buttons on page load
-$("document").ready( () => {
+// Attach event listeners to process and reset buttons on DOM load
+$("document").ready(() => {
     console.log("Ready!");
 
+    // Set current weight to empty weight on DOM load
+    $("#CurrentWeight").val(parseFloat($("#EmptyWeight").val()));
+
+    // Attach listeners to functional buttons
     $("#processCargoBtn").click(processCargo);
     $("#resetFormBtn").click(resetForm);
 });
